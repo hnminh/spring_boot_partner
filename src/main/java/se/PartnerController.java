13b28b9137b1546394e9b03df7519e9b081a8dfb
Hoang <e1800950@edu.vamk.fi>
2020-01-16 15:13:14 +0200

    Get one and POST working

--- src/main/java/se/PartnerController.java
+++ src/main/java/se/PartnerController.java
@@ -4,9 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @RestController
 public class PartnerController {
+    // this is how we use dependency injection (= object is created aytomatically)
 	@Autowired
 	private PartnerRepository repository;
     @RequestMapping("/test")
@@ -18,4 +23,14 @@ public class PartnerController {
     public Iterable<Partner> partners(@RequestParam(value="name", defaultValue="World") String name) {
         return repository.findAll();
     }    
+
+    @RequestMapping("/partner/{id}")
+    public Partner get(@PathVariable("id") int id) {
+        return repository.findOne(id);
+    }  
+
+    @RequestMapping(value = "/partner" , method = RequestMethod.POST)
+    public @ResponseBody Partner create(@RequestBody Partner item) {
+    	return repository.save(item);
+    }  
 }
